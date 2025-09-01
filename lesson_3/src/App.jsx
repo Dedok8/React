a822d60592c22b7303410ece2bbdc87e8988fb31
Dedok8 <korobchenko08082005@gmail.com>
2025-09-01 17:12:24 +0300

    lesson_3

--- lesson_3/src/App.jsx
+++ lesson_3/src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import styles from "./components/style/mainBlock.module.css";
+import MainBlock from "./components/MainBlock";
+import ConversionSm from "./components/conversionSm";
+import Temperature from "./components/Temperature";
+import Sapper from "./components/sapper";
+import ListSportsmens from "./components/ListSportsmens";
+import PairsList from "./components/PairsList";
+
+const gameObjField = [
+  {
+    id: 1,
+    mine: 0,
+    isOpen: false,
+  },
+  {
+    id: 2,
+    mine: 1,
+    isOpen: false,
+  },
+  {
+    id: 3,
+    mine: 1,
+    isOpen: false,
+  },
+  {
+    id: 4,
+    mine: 0,
+    isOpen: false,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={styles.container}>
+        <MainBlock title="Задача 1. З клавіатури вводиться довжина у сантиметрах. Виводити скільки це метрів, кілометрів.">
+          <ConversionSm />
+        </MainBlock>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className={styles.container}>
+        <MainBlock
+          title="Задача 2. З клавіатури вводиться температура. Змінювати колір фону у залежності від значення:"
+          description={
+            " менше нуля – білий, від 0 до 10 – синій, від 11 до 22 – зелений, вище 22 – червоний"
+          }
+        >
+          <Temperature />
+        </MainBlock>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className={styles.container}>
+        <MainBlock
+          title="Однорядковий сапер"
+          description={
+            "Однорядкова таблиця, до клітинок якої додано інформацію про наявність міни (використати атрибути). Спочатку клітинки сірі. При натисненні на клітинку аналізується чи є там міна і тоді колір стає червоним, якщо немає – зеленим. Додати можливість відкриття усіх сусідніх незамінованих клітинок при відкритті незамінованої клітинки."
+          }
+        >
+          <Sapper gameObjField={gameObjField} />
+        </MainBlock>
+      </div>
+      <div className={styles.container}>
+        <MainBlock
+          title="Задача 5"
+          description={
+            "Дано список спортсменів. Потрібно сформувати список тих, які будуть брати участь у змаганні. При цьому є два стовпці. В одному відображені всі спортсмени, в іншому – список тих, хто був вибраний. При натисканні на зелену стрілку спортсмен переміщається у список для змагань. При натисканні на червону стрілку спортсмен переміщається у загальний список."
+          }
+        >
+          <ListSportsmens />
+        </MainBlock>
+      </div>
+      <div className={styles.container}>
+        <MainBlock
+          title="Задача 6. Пари для танців"
+          description={
+            " Поступово вибираємо хлопця, дівчину і додаємо у обрані пари. Пару можна видалити. Поки не вибрано хлопця і дівчину кнопка «Додати» заблокована.  Якщо не вистачає хлопців або дівчат вибір також блокується."
+          }
+        >
+          <PairsList />
+        </MainBlock>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ lesson_3/src/components/ConversionSm.jsx
@@ -0,0 +1,28 @@
+// Задача 1. З клавіатури вводиться довжина у сантиметрах. Виводити скільки це метрів, кілометрів.
+
+import { useState } from "react";
+
+function ConversionSm() {
+  const [sm, setSm] = useState("");
+
+  const meters = sm ? (sm / 100).toFixed(2) : 0;
+  const kilometers = sm ? (sm / 100000).toFixed(5) : 0;
+
+  return (
+    <>
+      <label>
+        Введіть кількіть сантиметрів
+        <input
+          type="number"
+          value={sm}
+          onChange={(e) => setSm(Number(e.target.value))}
+          placeholder="Введіть кількіть сантиметрів "
+        />
+      </label>
+      <p>Кількість метрів: {meters} </p>
+      <p>Кількість кілометрів:{kilometers} </p>
+    </>
+  );
+}
+
+export default ConversionSm;

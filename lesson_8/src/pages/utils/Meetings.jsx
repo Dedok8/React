c1973e33cdf7ad3d965c99a7262630abe9b96c39
Dedok8 <korobchenko08082005@gmail.com>
2025-09-30 16:09:16 +0300

    update

--- lesson_8/src/pages/utils/Meetings.jsx
+++ lesson_8/src/pages/utils/Meetings.jsx
@@ -1,32 +1,47 @@
 import { useLocation, useNavigate } from "react-router";
 import TeachersCard from "../teachersComponent/components/TeachersCard.jsx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useFetch from "../../hook/useFetch.jsx";
 
 function Meetings() {
   const navigate = useNavigate();
+  const { state } = useLocation();
   const { data: teachersList = [], fetchTeacher, loading, error } = useFetch();
+  const [idTeacher, setIdTeacher] = useState([]);
 
   useEffect(() => {
-    fetchTeacher(); // подгружаем всех учителей с бэка
-  }, [fetchTeacher]);
+    fetchTeacher();
+    if (state?.idTeacher) {
+      setIdTeacher(state.idTeacher);
+      localStorage.setItem("selectTeachersId", JSON.stringify(state.idTeacher));
+    } else {
+      const save = localStorage.getItem("selectTeachersId");
+      if (save) setIdTeacher(JSON.parse(save));
+    }
+  }, [state, fetchTeacher]);
 
-  if (loading) return <div>Підтягуємо вчителів...</div>;
-  if (error) return <div>{error}</div>;
+  const teachersListId = teachersList.filter((teacher) =>
+    idTeacher.includes(String(teacher.id))
+  );
 
+  let content;
+
+  if (loading) return <div>Підтягуемо вчителів</div>;
+  else if (error) return <div>{error}</div>;
+
+  if (teachersListId.length > 0)
+    content = (
+      <div>
+        {teachersListId.map((teacher) => (
+          <TeachersCard teachers={teacher} key={teacher.id} />
+        ))}
+      </div>
+    );
+  else content = <h2>Вчителів нема</h2>;
   return (
     <div>
-      {teachersList.length > 0 ? (
-        <div>
-          {teachersList.map((teacher) => (
-            <TeachersCard teachers={teacher} key={teacher.id} />
-          ))}
-        </div>
-      ) : (
-        <h2>Вчителів нема</h2>
-      )}
-
-      <div style={{ marginTop: "20px" }}>
+      {content}
+      <div>
         <button type="button" onClick={() => navigate(-1)}>
           Назад до вибору вчителів
         </button>

3e1e5ae0f7b242ae0253ed35b551b68869bc4986
Dedok8 <korobchenko08082005@gmail.com>
2025-11-17 20:53:51 +0200

    update route

--- lesson_17/src/app/router/appRouterRoutes.js
+++ lesson_17/src/app/router/appRouterRoutes.js
@@ -1,10 +1,31 @@
-import { frontRoutes } from '@/shared/config/routes/frontRoutes'
+// import { frontRoutes } from '@/shared/config/routes/frontRoutes'
 
-const pagesList = Object.keys(frontRoutes.pages)
+// const pagesList = Object.keys(frontRoutes.pages)
+
+// export const appRouterRoutes = pagesList.map((page) => ({
+//   ...frontRoutes.pages[page],
+//   lazy: async () => ({
+//     Component: (await import(`../../pages/${page}`)).default,
+//   }),
+// }))
+
+import { frontRoutes } from "@/shared/config/routes/frontRoutes";
+
+const pages = import.meta.glob("../../pages/*.jsx");
+
+const pagesList = Object.keys(frontRoutes.pages);
 
 export const appRouterRoutes = pagesList.map((page) => ({
   ...frontRoutes.pages[page],
-  lazy: async () => ({
-    Component: (await import(`../../pages/${page}`)).default,
-  }),
-}))
+  lazy: async () => {
+    const importer = pages[`../../pages/${page}.jsx`];
+
+    if (!importer) {
+      throw new Error(`Page "${page}" не знайдена у /pages`);
+    }
+
+    return {
+      Component: (await importer()).default,
+    };
+  },
+}));
